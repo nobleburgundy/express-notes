d195245cbcc1d3603d8c7b7f66191d1523ed68a8
James Gould <nobleburgundy@gmail.com>
2020-12-14 15:06:46 -0600

    Reorganized some tests

--- tests/Note.test.js
+++ tests/Note.test.js
@@ -1,17 +1,12 @@
 const Note = require("../lib/Note");
-const NotesList = require("../lib/NoteList");
 const uuid = require("uuid");
 
-describe("Notes", () => {
+describe("Note Class", () => {
   describe("Initialization", () => {
     it("Should create an object with title and text when provided with correct arguments", () => {
-      const title = "Test";
-      const text = "Test text";
       const note = new Note("Test", "Test text");
 
-      expect(note.title).toEqual(title);
-      expect(note.text).toEqual(text);
-      expect(note.id.length).toBeGreaterThan(0);
+      expect(typeof note).toBe("object");
     });
 
     it("Should throw an error if no arugments are provided", () => {
@@ -20,65 +15,30 @@ describe("Notes", () => {
       expect(cb).toThrow();
     });
 
-    it("Note.id should generate a valid UUID", () => {
+    it("Note.getTitle() should return the correct title", () => {
+      // create a unique title to test against
+      const title = "testing" + uuid.v1();
+      const text = "testing";
+
+      const note = new Note(title, text);
+
+      expect(note.getTitle()).toEqual(title);
+    });
+
+    it("Note.getText() should return the correct title", () => {
+      const title = "testing";
+      // create unique text to test against
+      const text = "testing" + uuid.v1();
+
+      const note = new Note(title, text);
+
+      expect(note.getText()).toEqual(text);
+    });
+
+    it("Note.getId() should return a valid UUID", () => {
       const note = new Note("Test", "Test text");
 
-      expect(uuid.validate(note.id)).toBe(true);
-    });
-  });
-
-  describe("Add, Edit, Remove, Clear notes", () => {
-    it("Add single note", () => {
-      const noteList = new NotesList();
-      const note = new Note("testing", "testing text");
-      noteList.add(note);
-
-      expect(noteList.notes).toContainEqual(note);
-    });
-
-    it("Add multiple notes", () => {
-      const noteList = new NotesList();
-      const note = new Note("testing", "testing text");
-      const note2 = new Note("testing", "testing text");
-
-      noteList.add(note);
-      noteList.add(note2);
-
-      expect(noteList.notes).toContainEqual(note);
-      expect(noteList.notes).toContainEqual(note2);
-    });
-
-    it("Edit note", () => {
-      const noteList = new NotesList();
-      const note = new Note("Delete this note", "Delete this note text");
-      const newText = "This is the great new edited text";
-
-      noteList.add(note);
-      noteList.editNote(note.id, newText);
-
-      expect(noteList.notes).toContainEqual(note);
-    });
-
-    it("Remove note", () => {
-      const noteList = new NotesList();
-      const note = new Note("Delete this note", "Delete this note text");
-
-      noteList.add(note);
-      noteList.remove(note);
-
-      expect(noteList.notes).not.toContainEqual(note);
-    });
-
-    it("Clear notes", () => {
-      const noteList = new NotesList();
-      const note = new Note("testing", "testing text");
-      const note2 = new Note("testing", "testing text");
-      noteList.add(note);
-      noteList.add(note2);
-
-      noteList.clear();
-
-      expect(noteList.notes.length).toBe(0);
+      expect(uuid.validate(note.getId())).toBe(true);
     });
   });
 });
---
+++ tests/NoteList.test.js
@@ -0,0 +1,66 @@
+const Note = require("../lib/Note");
+const NoteList = require("../lib/NoteList");
+
+describe("NoteList class", () => {
+  describe("Initilization", () => {
+    it("Should create an object when initialized", () => {
+      const noteList = new NoteList();
+
+      expect(typeof noteList).toBe("object");
+    });
+  });
+  describe("Add, Edit, Remove, Clear notes", () => {
+    it("Add single note", () => {
+      const noteList = new NoteList();
+      const note = new Note("testing", "testing text");
+      noteList.add(note);
+
+      expect(noteList.notes).toContainEqual(note);
+    });
+
+    it("Add multiple notes", () => {
+      const noteList = new NoteList();
+      const note = new Note("testing", "testing text");
+      const note2 = new Note("testing", "testing text");
+
+      noteList.add(note);
+      noteList.add(note2);
+
+      expect(noteList.notes).toContainEqual(note);
+      expect(noteList.notes).toContainEqual(note2);
+    });
+
+    it("Edit note", () => {
+      const noteList = new NoteList();
+      const note = new Note("Delete this note", "Delete this note text");
+      const newText = "This is the great new edited text";
+
+      noteList.add(note);
+      noteList.editNote(note.id, newText);
+
+      expect(noteList.notes).toContainEqual(note);
+    });
+
+    it("Remove note", () => {
+      const noteList = new NoteList();
+      const note = new Note("Delete this note", "Delete this note text");
+
+      noteList.add(note);
+      noteList.remove(note);
+
+      expect(noteList.notes).not.toContainEqual(note);
+    });
+
+    it("Clear notes", () => {
+      const noteList = new NoteList();
+      const note = new Note("testing", "testing text");
+      const note2 = new Note("testing", "testing text");
+      noteList.add(note);
+      noteList.add(note2);
+
+      noteList.clear();
+
+      expect(noteList.notes.length).toBe(0);
+    });
+  });
+});
