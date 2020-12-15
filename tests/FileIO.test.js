f24606feb7ec628f5334a077859b5381a115f08f
James Gould <nobleburgundy@gmail.com>
2020-12-15 09:50:57 -0600

    Refactored FileIO to class and moved file path to that class.

--- tests/FileIO.test.js
+++ tests/FileIO.test.js
@@ -1,18 +1,18 @@
 const FileIO = require("../lib/FileIO");
 const fs = require("fs");
-
+const path = require("path");
 jest.mock("fs");
 
 describe("FileIO", () => {
   describe("read", () => {
     it("should call fs.readFileSync with the passed in 'file' argument", () => {
       const fileIO = new FileIO();
-      const file = "db.json";
+      const file = path.resolve(path.join("./db", "db.json"));
       let data;
       const mockReturnData = JSON.stringify({ title: "Test", text: "Testing" });
 
       fs.readFileSync.mockReturnValue(mockReturnData);
-      data = fileIO.read(file);
+      data = fileIO.read();
 
       expect(data).toEqual(mockReturnData);
       expect(fs.readFileSync).lastCalledWith(file, "utf8");
@@ -22,22 +22,22 @@ describe("FileIO", () => {
   describe("write", () => {
     it("should call fs.writeFileSync with the passed in 'path' and 'data' arguments", () => {
       const fileIO = new FileIO();
-      const path = "../db/db.json";
+      const file = path.resolve(path.join("./db", "db.json"));
       const data = JSON.stringify({ title: "Test", text: "Testing" });
 
-      fileIO.write(path, data);
+      fileIO.write(data);
 
-      expect(fs.writeFileSync).lastCalledWith(path, data);
+      expect(fs.writeFileSync).lastCalledWith(file, data);
     });
   });
 
   describe("append", () => {
     it("should call fs.appendFileSync with the passed in 'file' and 'data' arguments", () => {
       const fileIO = new FileIO();
-      const file = "../db/db.json";
+      const file = path.resolve(path.join("./db", "db.json"));
       const data = JSON.stringify({ title: "Test", text: "Testing" });
 
-      fileIO.append(file, data);
+      fileIO.append(data);
 
       expect(fs.appendFileSync).lastCalledWith(file, data);
     });
