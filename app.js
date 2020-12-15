c2b3e8292c6f06e42546d2475c04082279ea1219
James Gould <nobleburgundy@gmail.com>
2020-12-15 10:24:23 -0600

    Changes to app.js to troubleshoot heroku crash

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/notes", (req, res) => {
